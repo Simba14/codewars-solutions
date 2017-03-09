09499c03e3262eceb792bab74fc6e08959e460cf
Sean Blundell <seanblundell@gmail.com>
2017-03-09 16:39:03 +0000

    Jaden casing strings kata completed

---
+++ 7-kyu/jaden_casing_strings.rb
@@ -0,0 +1,21 @@
+=begin Instructions:
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+=end
+
+class String
+  def toJadenCase
+    self.split(" ").map(&:capitalize).join(" ")
+  end
+end
+
+str = "How can mirrors be real if our eyes aren't real"
+puts str.toJadenCase()
+
+# Best practice solution:
+# class String
+#  def toJadenCase
+#    self.split.map(&:capitalize).join(" ")
+#  end
+# end
